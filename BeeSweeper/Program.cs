3d5d883f0f827ad91c5ebf6afc23ff7dd788cd45
sharphurt <sharphurt@users.noreply.github.com>
2020-05-06 16:27:42 +0500

    Remove useless files

--- BeeSweeper/Program.cs
+++ BeeSweeper/Program.cs
@@ -14,7 +14,7 @@ namespace BeeSweeper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+         //   Application.Run(new Form1());
         }
     }
 }
